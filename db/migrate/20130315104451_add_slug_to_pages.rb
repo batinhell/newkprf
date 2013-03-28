81f7dc817a6b1beed0a7b66beae66a850b8cb64d
batinhell <batinhell@gmail.com>
2013-03-28 18:47:28 +0400

    add logo and search

--- db/migrate/20130315104451_add_slug_to_pages.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugToPages < ActiveRecord::Migration
-  def change
-    add_column :pages, :slug_en, :string
-    add_index :pages, :slug_en
-  end
-end
--- db/migrate/20130315111814_add_slugru_to_pages.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugruToPages < ActiveRecord::Migration
-  def change
-    add_column :pages, :slug_ru, :string
-    add_index :pages, :slug_ru
-  end
-end
