57b3655345fedb2f853a4345db5865f8523b8422
batcat <batinhell@gmail.com>
2013-04-17 02:09:11 +0400

    13

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,13 +8,13 @@ set :default_environment, {
   'RUBY_VERSION' => 'ruby 1.9.3',
   'GEM_HOME'     => '/home/kprf/data/.rvm/gems/ruby-1.9.3-p392@global',
   'GEM_PATH'     => '/home/kprf/data/.rvm/gems/ruby-1.9.3-p392@global',
-  'BUNDLE_PATH'  => '/home/kprf/data/.rvm/gems/ruby-1.9.3-p392'  # If you are using bundler.
+  #'BUNDLE_PATH'  => '/home/kprf/data/.rvm/gems/ruby-1.9.3-p392'  # If you are using bundler.
 }
 
 
 
-set :bundle_cmd, "/home/kprf/data/.rvm/gems/ruby-1.9.3-p392@newkprf/bin/bundle"
-set :bundle_dir, "/home/kprf/data/.rvm/gems/ruby-1.9.3-p392"
+# set :bundle_cmd, "/home/kprf/data/.rvm/gems/ruby-1.9.3-p392@newkprf/bin/bundle"
+# set :bundle_dir, "/home/kprf/data/.rvm/gems/ruby-1.9.3-p392"
 require "bundler/capistrano"
 
 server "77.246.150.198", :web, :app, :db, primary: true
