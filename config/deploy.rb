02e20393f54e3a60930fd4dacda07336d75753ea
batinhell <batinhell@gmail.com>
2013-04-22 12:46:23 +0400

    qwe

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+      run "/usr/local/etc/rc.d/unicorn_#{application} #{command}"
     end
   end
 
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 root = "/home/kprf/data/www/kprf-arh.ru/current"
-working_directory root
+#working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
