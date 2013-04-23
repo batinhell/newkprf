25a67c947c3c973bdb94ad8b8925b21c477fa4de
batinhell <batinhell@gmail.com>
2013-04-23 18:10:06 +0400

    nah

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
   task :restart do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
   end
   task :start do
     run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
