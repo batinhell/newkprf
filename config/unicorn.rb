e9a0f8afae027946cada2f04acf2513444b02295
batinhell <batinhell@gmail.com>
2013-04-23 16:05:07 +0400

    fix unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 working_directory "~/www/kprf-arh.ru/current"
 pid "/home/kprf/data/www/kprf-arh.ru/current/tmp/pids/unicorn.pid"
-stderr_path "/home/kprf/data/logs/stderr.log"
+stderr_path "/home/kprf/data/www/kprf-arh.ru/current/log/stderr.log"
 listen "/tmp/unicorn.kprf.sock"
 worker_processes 2
 timeout 30
