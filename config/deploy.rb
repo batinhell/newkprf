45c3908d7d44ba1c5e92fe6a415b75541a76688b
batinhell <batinhell@gmail.com>
2013-04-23 11:50:20 +0400

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,4 +45,12 @@ namespace :deploy do
   task :stop do
     run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
   end
+
+  task :setup_config, roles: :app do
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
 end
