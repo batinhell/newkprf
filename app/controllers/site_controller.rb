bc9f1d40983d43ec07857326137ed37c80a790ca
batcat <batinhell@gmail.com>
2013-05-13 15:16:43 +0400

    edit pagination

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,7 @@
 class SiteController < ApplicationController
   def index
     # @posts = Post.limit(6)
-    @posts = Post.page(params[:page]).per(6)
+    @posts = Post.page(params[:page]).per(8)
     @slider = Post.slider
     render :layout => 'index'
   end
