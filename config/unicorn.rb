d08404a1f3c0267d6502f49e227cb0fef7227cac
batcat <batinhell@gmail.com>
2013-05-19 18:15:53 +0400

    modify unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 working_directory "~/www/kprf-arh.ru"
 pid "/home/kprf/data/www/kprf-arh.ru/tmp/pids/unicorn.pid"
-stderr_path "/home/kprf/data/logs/stderr.log"
+# stderr_path "/home/kprf/data/logs/stderr.log"
 listen "/tmp/unicorn.kprf.sock"
 worker_processes 2
 timeout 30
